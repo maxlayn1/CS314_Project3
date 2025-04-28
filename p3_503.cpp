fafa55785eaca246ae4b919fd311a2af2b7ec0e0
maxlayn1 <maxlayn1@gmail.com>
2025-04-28 05:22:02 -0500

    basic structure for the program done?

---
+++ p3_503.cpp
@@ -0,0 +1,131 @@
+/* ********************************************************** *
+ * CS314 Project #3 solution                                  *
+ *                                                            *
+ * Your last-three: 503                                       *
+ * Your course section #: 001                                 *
+ *                                                            *
+ * Spring 2025                                                *
+ *                                                            *
+ * ********************************************************** */
+
+#define SLOWDOWN_FACTOR    6000000     /* child thread display slowdown */
+#define SCHEDULE_INTERVAL  1           /* scheduling interval in second */
+
+#include <string.h>                     /* for strerror()                */
+#include <stdio.h>                      // for printf ()                 
+#include <signal.h>                     // for interrupt handling     
+#include <unistd.h>                     // for "alarm"                 
+#include <pthread.h>                    // pthread implementation      
+
+/* prototypes --------------------------------------------------------- */
+void child_thread_routine (void * arg); // definition of a chile thread
+void clock_interrupt_handler(void);
+
+/* global variables ---------------------------------------------------*/
+pthread_mutex_t  condition_mutex;  // pthread mutex_condition 
+pthread_cond_t   t_condition;      // pthread condition
+int schedule_vector[5];            // the required thread schedule vector
+int loop_counter;                  // loop counter for the interrupt handler
+
+/* the main (the parent thread) --------------------------------------- */
+int main(void)
+{
+    /* thread attributes for managing child threads ------------------- */
+    pthread_t        thread;       // thread IDs (assigned by OS)
+    pthread_attr_t     attr;       // thread attributes 
+    int                 ids;       // thread arguments
+
+    /* initialize the schedule vector --------------------------------- */
+    schedule_vector[0] = 0;        // the first thread
+    schedule_vector[1] = 1;        // the second thread
+    schedule_vector[2] = 2;        // the third thread
+    schedule_vector[3] = 3;        // the fourth thread
+    schedule_vector[4] = 4;        // the fifth thread
+    int i = 0;                     // the loop counter for the parent thread  
+    loop_counter = 0;              // initialize the loop counter for the interrupt handler
+
+    /* specify the clock interrupt to be sent to this process --- */
+    signal(SIGALRM, (void *) (clock_interrupt_handler));
+    /* set the interrupt interval to 1 second --- */
+    alarm(SCHEDULE_INTERVAL);
+
+    /* initialize pthread mutex --------------------------------------- */
+    pthread_mutex_init (&condition_mutex, NULL);
+
+    /* initialize pthread condition variable -------------------------- */
+    pthread_cond_init (&t_condition, NULL);
+
+    /* create a child thread ------------------------------------------ */
+    ids = 0;                         // thread ID = 0
+    pthread_create (                 // xreate a child thread   
+               &thread,              // thread ID (system assigned)
+               NULL,                 // default thread attributes
+               (void *) (child_thread_routine), // thread routine name
+               &ids);                // arguments to be passed
+
+    /* inifinite loop for the parent thread --------------------------- */
+    printf("   the parent thread starts looping ....\n");
+    while (1)
+    {
+       /* the parent thread waits for one second -------------- */
+       sleep (1);
+
+       /* the parent thread unlock the condition variable ----- */
+       printf("        the parent thread signals the child thread ...\n");
+       pthread_mutex_lock(&condition_mutex);
+          pthread_cond_signal(&t_condition);
+       pthread_mutex_unlock(&condition_mutex);
+    }
+
+    /* The main (parent) thread terminates itself ------------------ */
+    return(0);
+}
+/* THE END OF MAIN ===================================================== */
+
+/* child thread implementation ---------------------------------------- */
+void child_thread_routine (void * arg)  
+{
+   int  myid = *(int *) arg;   // child thread number (not ID)
+   int  my_counter = 0;        // local loop counter
+
+   /* -------------------------------------------------------- */
+   /* if you would like to add your local variable(s), you can */
+   /* add one (them) here.                                     */
+   /* -------------------------------------------------------- */
+
+   /* declarare the start of this child thread (required) ---- */
+   printf("Child thread %d started ...\n", myid);   
+
+   /* -------------------------------------------------------- */
+   /* if you would like to peform some initialization to your  */
+   /*  local variable(s), you can do so here.                  */
+   /* -------------------------------------------------------- */
+
+   /* infinite loop (required) ------------------------------- */
+   while (1)
+   {
+      /* declare working of myself (required) ---------------- */
+      my_counter ++;
+      if ((my_counter % SLOWDOWN_FACTOR) == 0)
+      {  printf("Thread: %d is running ...\n", myid);  }
+
+   /* -------------------------------------------------------- */
+   /* Here is your working space (to implement some mechanism) */
+   /* to synchronize with the interrupt handeler.              */
+   /* -------------------------------------------------------- */
+   }
+}
+/* END OF A CHILD THREAD ============================================== */
+
+/* The interrupt handler for SIGALM interrupt ---------------------- */
+void clock_interrupt_handler(void)
+{
+   /* to be displayed at each time I woke up ---------------- */
+   printf("I woke up on the timer interrupt (%d) .... \n", loop_counter);
+
+   /* increase the loop counter for the interrupt handler --- */
+   loop_counter = loop_counter + 1;
+
+   /* scheduler wakes up again one second later -------------------- */  
+   alarm(SCHEDULE_INTERVAL);
+}
