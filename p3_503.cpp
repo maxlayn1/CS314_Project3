f240b6bd001521318e88b3e87d062f06ac202433
maxlayn1 <maxlayn1@gmail.com>
2025-04-28 10:15:57 -0500

    added barrier to allow each thread to start before they run

--- p3_503.cpp
+++ p3_503.cpp
@@ -8,8 +8,9 @@
  *                                                            *
  * ********************************************************** */
 
-#define SLOWDOWN_FACTOR    6000000     /* child thread display slowdown */
+#define SLOWDOWN_FACTOR    60000000     /* child thread display slowdown was 6000000*/
 #define SCHEDULE_INTERVAL  1           /* scheduling interval in second */
+#define THREAD_COUNT 5
 
 #include <string.h>                     /* for strerror()                */
 #include <stdio.h>                      // for printf ()                 
@@ -22,39 +23,38 @@ void clock_interrupt_handler(int signum);
 
 pthread_mutex_t  condition_mutex;  // pthread mutex_condition 
 pthread_cond_t   t_condition;      // pthread condition
+pthread_barrier_t barrier;         // barrier to sync threads
 int schedule_vector[5];            // the required thread schedule vector
 int loop_counter;                  // loop counter for the interrupt handler
+int thread_count = 0;
 
 int main(void)
 {
-    pthread_t        thread;       // thread IDs (assigned by OS)
-    pthread_attr_t     attr;       // thread attributes 
-    int                 ids;       // thread arguments
+    pthread_t       threads[THREAD_COUNT];       // thread IDs (assigned by OS)
+    pthread_attr_t     attr[THREAD_COUNT];       // thread attributes 
+    int                 ids[THREAD_COUNT];       // thread arguments
 
     schedule_vector[0] = 0;        // the first thread
     schedule_vector[1] = 1;        // the second thread
     schedule_vector[2] = 2;        // the third thread
     schedule_vector[3] = 3;        // the fourth thread
     schedule_vector[4] = 4;        // the fifth thread
-    int parent_loop_counter = 5;   // the loop counter for the parent thread  
+
+    int parent_loop_counter = THREAD_COUNT;   // the loop counter for the parent thread  
     int interrupt_loop_counter = 0;// initialize the loop counter for the interrupt handler
 
-    /* specify the clock interrupt to be sent to this process --- */
-    signal(SIGALRM, clock_interrupt_handler);
-    /* set the interrupt interval to 1 second --- */
-    alarm(SCHEDULE_INTERVAL);
+    signal(SIGALRM, clock_interrupt_handler); // specify the clock interrupt to be sent to this process
+    alarm(SCHEDULE_INTERVAL); // set the interrupt interval to 1 second
 
     pthread_mutex_init (&condition_mutex, NULL);
     pthread_cond_init (&t_condition, NULL);
+    pthread_barrier_init(&barrier, NULL, THREAD_COUNT); // initialize barrier to sync 5 threads
 
-    ids = 0;                         // thread ID = 0
-    pthread_create (                 // create a child thread   
-               &thread,              // thread ID (system assigned)
-               NULL,                 // default thread attributes
-               child_thread_routine, // thread routine name
-               &ids);                // arguments to be passed
+    for (int i = 0; i < parent_loop_counter; i++) {
+        ids[i] = i;
+        pthread_create(&threads[i], NULL, child_thread_routine, &ids[i]);
+    }
 
-    printf("   the parent thread starts looping ....\n");
     while (1)
     {
        sleep (1); // wait for one second
@@ -72,18 +72,16 @@ void *child_thread_routine (void * arg)
    int  myid = *(int *) arg;   // child thread number (not ID)
    int  my_counter = 0;        // local loop counter
 
-   /* -------------------------------------------------------- */
-   /* if you would like to add your local variable(s), you can */
-   /* add one (them) here.                                     */
-   /* -------------------------------------------------------- */
-
    /* declare the start of this child thread (required) ---- */
+   thread_count = thread_count + 1;
    printf("Child thread %d started ...\n", myid);   
 
-   /* -------------------------------------------------------- */
-   /* if you would like to peform some initialization to your  */
-   /*  local variable(s), you can do so here.                  */
-   /* -------------------------------------------------------- */
+   // Wait until all threads reach this point
+    pthread_barrier_wait(&barrier);
+
+   while (thread_count != THREAD_COUNT) {
+    sleep(1);
+   }
 
    /* infinite loop (required) ------------------------------- */
    while (1)
@@ -93,10 +91,7 @@ void *child_thread_routine (void * arg)
       if ((my_counter % SLOWDOWN_FACTOR) == 0)
       {  printf("Thread: %d is running ...\n", myid);  }
 
-   /* -------------------------------------------------------- */
-   /* Here is your working space (to implement some mechanism) */
-   /* to synchronize with the interrupt handler.               */
-   /* -------------------------------------------------------- */
+      
    }
 }
 
